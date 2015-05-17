51146d0ce70bd3b8c59cf0fdb07fd98398b2b8bd
Brian Carper <brian@carper.ca>
2015-05-17 16:29:08 -0700

    lein config

---
+++ .lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [[lein-pprint "1.1.1"]
+                  [lein-immutant "1.2.2"]
+                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}

182cc3dd5241d240a9a68be97ac3c2bedb5c5f91
Brian Carper <brian@carper.ca>
2019-10-16 14:33:06 -0700

    more ubuntu packages

--- setup-ubuntu.zsh
+++ setup-ubuntu.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
-sudo apt install git zsh ripgrep fd-find neovim docker docker-compose fzf python3 python-dev python-pip python3-dev python3-pip tldr
-sudo add-apt-repository -y ppa:lazygit-team/release
-sudo apt install lazygit
-sudo add-apt-repository -y ppa:neovim-ppa/stable
+sudo apt install git zsh ripgrep fd-find neovim docker docker-compose fzf python3 python-dev \
+    python-pip python3-dev python3-pip tldr i3-gaps npm tmux feh
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt install neovim
+

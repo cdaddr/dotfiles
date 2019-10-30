6fd9a047dd303cf27c587a70f66efab9c02f31dc
Brian Carper <brian@carper.ca>
2019-10-30 13:35:00 -0700

    more ubuntu installs

--- setup-ubuntu.zsh
+++ setup-ubuntu.zsh
@@ -1,6 +1,8 @@
 #!/bin/zsh
-sudo apt install git zsh ripgrep fd-find neovim docker docker-compose fzf python3 python-dev \
-    python-pip python3-dev python3-pip tldr i3-gaps npm tmux feh
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo apt install neovim
-
+sudo apt install -y git zsh docker docker-compose \
+    python3 python-dev python-pip python3-dev python3-pip python-openssl \
+    i3-gaps npm tmux feh hsetroot fzf tldr ripgrep fd-find neovim \
+    make build-essential libssl-dev zlib1g-dev libbz2-dev \
+    libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+    xz-utils tk-dev libffi-dev liblzma-dev

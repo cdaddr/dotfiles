add3eb882e72ae1696fa672292017802c982d49f
Brian Carper <brian@carper.ca>
2020-05-17 18:13:54 -0700

    remove unused

--- setup.zsh
+++ setup.zsh
@@ -16,37 +16,20 @@ mkdir "$HOME/.config" &>/dev/null
 lns "$DOTFILES/gitconfig"               "$HOME/.gitconfig"
 lns "$DOTFILES/gitignore"               "$HOME/.gitignore"
 lns "$DOTFILES/ignore"                  "$HOME/.ignore"
-lns "$DOTFILES/spacemacs"               "$HOME/.spacemacs"
 lns "$DOTFILES/tmux.conf"               "$HOME/.tmux.conf"
-lns "$DOTFILES/vim"                     "$HOME/.vim"
-lns "$DOTFILES/vimrc"                   "$HOME/.vimrc"
 lns "$DOTFILES/zsh"                     "$HOME/.zsh"
 lns "$DOTFILES/zshrc"                   "$HOME/.zshrc"
-lns "$DOTFILES/config/kitty/"           "$HOME/.config/kitty"
 lns "$DOTFILES/config/nvim"             "$HOME/.config/nvim"
 lns "$DOTFILES/config/i3"               "$HOME/.config/i3"
-lns "$DOTFILES/config/alacritty"        "$HOME/.config/alacritty"
 lns "$DOTFILES/dir_colors"              "$HOME/.dir_colors"
 lns "$DOTFILES/i3status.conf"           "$HOME/.i3status.conf"
 lns "$DOTFILES/rsync.excludes"          "$HOME/.rsync.excludes"
-mkdir -p "$HOME/.vim/backup"
-mkdir -p "$HOME/.vim/undo"
 
 tic -o ~/.terminfo xterm-256color-italic.terminfo
 tic -o ~/.terminfo tmux-256color.terminfo
 
 log "Sourcing zshrc..."
 source ~/.zshrc
-
-log "Setting up go (GOPATH=$GOPATH)..."
-if [ -x "$(command -v go)" ]; then
-    mkdir -p $GOPATH
-    goget github.com/stamblerre/gocode
-    goget github.com/rogpeppe/godef
-    goget golang.org/x/tools/cmd/guru
-    goget golang.org/x/tools/cmd/gorename
-    goget golang.org/x/tools/cmd/goimports
-fi
 
 log "Fetching Vim plugins..."
 nvim +"PlugInstall | qall"
@@ -58,15 +41,6 @@ log "* python"
 python3 -m pip install --user --upgrade pynvim
 log "* node"
 npm install -g neovim
-
-EMACS="$HOME/.emacs.d"
-if [[ -e $EMACS ]]; then
-    log "Updating Spacemacs..."
-    git -C $EMACS pull
-else
-    log "Cloning Spacemacs..."
-    git clone -b develop https://github.com/syl20bnr/spacemacs $EMACS
-fi
 
 if [[ `uname` == 'Darwin' ]]; then
     note "You might want to go to https://brew.sh/ and install homebrew."

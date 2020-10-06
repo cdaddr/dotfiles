dea373ccf2b3934e83cceb6b27a37cee6cef228c
Brian Carper <brian@carper.ca>
2020-10-06 14:27:47 -0700

    remove macos setup - use standard setup

--- setup-macos.zsh
+++
@@ -1,5 +0,0 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install pyenv
-pyenv install 3.8.0
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
-nvm install stable

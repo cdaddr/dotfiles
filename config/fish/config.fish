b8d0021cffff4bc89dcb0bc9c955948647ebfe6f
Brian Carper <brian@carper.ca>
2020-10-08 13:24:45 -0700

    fish stuff

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,16 @@
-status --is-interactive; and source (pyenv init -|psub)
-status --is-interactive; and source (rbenv init -|psub)
+
+source $HOME/.config/fish/aliases.fish
+
+set -x EDITOR nvim
+set -x PAGER less
+set -x FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --exclude .git --exclude '*~'"
+set -x XDG_DATA_HOME "$HOME/.local/share"
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x XDG_CACHE_HOME "$HOME/.cache"
+set -x PATH $HOME/.bin $PATH
+set NVM_DIR "$HOME/.nvm"
+
+status --is-interactive && source (pyenv init -|psub)
+status --is-interactive && source (rbenv init -|psub)
+status --is-interactive && source (jenv init  -|psub)
+

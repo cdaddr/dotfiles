5cf2d8c70d5d9b2d6f1f329e463b6dd30afa9a0c
Brian Carper <brian@carper.ca>
2026-01-20 13:45:19 -0800

    nvim reenable persisted close scratch buffers on exit

--- config/nvim/lua/plugins/persisted.lua
+++ config/nvim/lua/plugins/persisted.lua
@@ -11,26 +11,26 @@ return {
       "winsize",
     }
     -- close everything I don't want to save to the session
-    -- vim.api.nvim_create_autocmd("ExitPre", {
-    --   callback = function()
-    --     local buftypes_to_close = { "help", "quickfix", "nofile", "terminal", "prompt" }
-    --     local filetypes_to_close = { "", "neo-tree", "oil", "toggleterm", "notify" }
-    --
-    --     for _, win in ipairs(vim.api.nvim_list_wins()) do
-    --       local buf = vim.api.nvim_win_get_buf(win)
-    --       local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
-    --       local filetype = vim.api.nvim_get_option_value("filetype", { buf = buf })
-    --
-    --       if vim.tbl_contains(buftypes_to_close, buftype) or
-    --         vim.tbl_contains(filetypes_to_close, filetype) then
-    --         local success, err = pcall(vim.api.nvim_win_close, win, false)
-    --         if not success then
-    --           vim.notify(err)
-    --         end
-    --       end
-    --     end
-    --   end,
-    -- })
+    vim.api.nvim_create_autocmd("ExitPre", {
+      callback = function()
+        local buftypes_to_close = { "help", "quickfix", "nofile", "terminal", "prompt" }
+        local filetypes_to_close = { "", "neo-tree", "oil", "toggleterm", "notify" }
+
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
+          local filetype = vim.api.nvim_get_option_value("filetype", { buf = buf })
+
+          if vim.tbl_contains(buftypes_to_close, buftype) or
+            vim.tbl_contains(filetypes_to_close, filetype) then
+            local success, err = pcall(vim.api.nvim_win_close, win, false)
+            if not success then
+              vim.notify(err)
+            end
+          end
+        end
+      end,
+    })
     require('persisted').setup({
       autoload = true,  -- automatically load session for the cwd
       autosave = true,  -- automatically save session on exit

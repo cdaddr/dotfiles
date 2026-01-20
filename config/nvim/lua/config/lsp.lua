5ad9090c4044d0994dc550598146b2b9a61844dd
Brian Carper <brian@carper.ca>
2026-01-20 13:44:07 -0800

    nvim lsp mapping

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -66,6 +66,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<leader>lt', vim.lsp.buf.type_definition, {buffer = true, desc = "Type definition"})
     vim.keymap.set('n', '<leader>lr', vim.lsp.buf.references, {buffer = true, desc = "References"})
     vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float, {noremap = true, buffer = true, desc = "Diagnostic float"})
+    vim.keymap.set('n', '<leader>K', vim.diagnostic.open_float, {noremap = true, buffer = true, desc = "Diagnostic float"})
     for _, key in ipairs({'<leader>lE', '<F2>'}) do
       vim.keymap.set('n', key, function()
         vim.diagnostic.setqflist({open = false})

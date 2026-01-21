d2fda4d429fadc7121ad108425d7acc237d1b067
Brian Carper <brian@carper.ca>
2026-01-21 15:32:40 -0800

    nvim highlight oil windows

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -4,9 +4,9 @@ return {
   opts = {
     default_file_explorer = true,
     columns = {
-      "permissions",
-      "size",
-      "mtime",
+      { "permissions", highlight = "String" },
+      { "mtime", highlight = "Keyword" },
+      { "size", align = "left", highlight = "Constant" },
       "icon",
     },
     confirmation = {
@@ -18,10 +18,10 @@ return {
     keymaps = {
       -- ["<esc>"] = { "actions.close", mode = "n" },
       ["q"] = { "actions.close", mode = "n" },
-    }
+    },
   },
   keys = {
     { "<Leader>o", "<cmd>Oil<cr>", desc = "Open Oil" },
   },
-  lazy = false
+  lazy = false,
 }

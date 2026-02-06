d9f6c1081f1aba6f9970ed0654f853450cbe693d
Brian Carper <brian@carper.ca>
2026-02-06 13:54:26 -0800

    nvim disable lua auto-reading, for perf

--- config/nvim/lua/plugins/lazydev.lua
+++ config/nvim/lua/plugins/lazydev.lua
@@ -9,16 +9,13 @@ return {
         -- See the configuration section for more details
         -- Load luvit types when the `vim.uv` word is found
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        "LazyVim",
-        { path = "wezterm-types", mods = { "wezterm" } },
-        { path = "Snacks" },
       }
-      local files = io.popen("fd --type d --exact-depth 2 lua ~/.local/share/nvim/lazy/")
-      if files then
-        for file in files:lines() do
-          library[#library + 1] = file
-        end
-      end
+      -- local files = io.popen("fd --type d --exact-depth 2 lua ~/.local/share/nvim/lazy/")
+      -- if files then
+      --   for file in files:lines() do
+      --     library[#library + 1] = file
+      --   end
+      -- end
       return {
         library = library,
       }

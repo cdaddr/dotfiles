fbc098cb044337241e79a3c3932451a367dbe360
Brian Carper <brian@carper.ca>
2026-02-09 14:00:25 -0800

    nvim enable experimental _extui

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -22,3 +22,14 @@ require("config.autocmds")
 vim.cmd.colorscheme(theme.nvim)
 
 vim.diagnostic.config({ virtual_text = true, virtual_lines = false, underline = true, signs = false })
+
+-- experimental
+require("vim._extui").enable({
+  enable = true, -- Whether to enable or disable the UI.
+  msg = { -- Options related to the message module.
+    ---@type 'cmd'|'msg' Where to place regular messages, either in the
+    ---cmdline or in a separate ephemeral message window.
+    target = "cmd",
+    timeout = 4000, -- Time a message is visible in the message window.
+  },
+})

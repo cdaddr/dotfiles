bb67f9bf1224aa9e38ac6fd0c4c2786ecea19111
Brian Carper <brian@carper.ca>
2026-02-11 11:04:07 -0800

    zsh install script better conflict handling

--- install.rb
+++ install.rb
@@ -13,25 +13,12 @@ end
 HOME = ENV['HOME']
 DOTFILES_DIR = File.expand_path(File.dirname(__FILE__))
 
-def link_file(source_path, target_path)
-  FileUtils.mkdir_p(File.dirname(target_path))
-
-  if File.exist?(target_path) && !File.symlink?(target_path)
-    backup_path = "#{target_path}.backup"
-    puts "Backing up existing #{target_path} to #{backup_path}".yellow
-    FileUtils.mv(target_path, backup_path)
-  end
-
-  FileUtils.ln_sf(source_path, target_path)
-  puts "Linked #{source_path} to #{target_path}".green
-end
-
-%w[
+PATHS = %w[
   .config/aliases.sh
+  .config/bash
   .config/bat
   .config/dircolors
   .config/eza
-  .config/fish
   .config/ghostty
   .config/git
   .config/ideavim
@@ -49,14 +36,28 @@ end
   .config/current-theme-env.zsh
   .config/current-theme.lua
   .config/LS_COLORS.sh
-  .config/Brewfile
   .hushlogin
   .zshrc
-].each do |path|
-    source_path = File.join(DOTFILES_DIR, path.un_dot)
-    target_path = File.join(HOME, path)
-    link_file(source_path, target_path)
-  end
+]
+
+conflicts = PATHS.filter_map do |path|
+  target_path = File.join(HOME, path)
+  target_path if File.exist?(target_path) && !File.symlink?(target_path)
+end
+
+if conflicts.any?
+  puts "Conflicts found! Remove these before running install:".red
+  conflicts.each { |path| puts "  #{path}".yellow }
+  exit 1
+end
+
+PATHS.each do |path|
+  source_path = File.join(DOTFILES_DIR, path.un_dot)
+  target_path = File.join(HOME, path)
+  FileUtils.mkdir_p(File.dirname(target_path))
+  FileUtils.ln_sf(source_path, target_path)
+  puts "Linked #{target_path}".green
+end
 
 puts "Setup complete!".green
 

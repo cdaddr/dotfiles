927f3d7326a9b25a7cb83323ac197494778ba31d
Brian Carper <brian@carper.ca>
2026-02-23 16:45:34 -0800

    nvim better quickfix display

--- config/nvim/lua/plugins/nvim-bqf.lua
+++ config/nvim/lua/plugins/nvim-bqf.lua
@@ -20,32 +20,51 @@ return {
       else
         items = fn.getloclist(info.winid, { id = info.id, items = 0 }).items
       end
-      local limit = 48
-      local fnameFmt1, fnameFmt2 = "%-" .. limit .. "s", "…%." .. (limit - 1) .. "s"
-      local validFmt = "%s │%5d:%-3d│%s %s"
+      local maxLen = 64
+
+      -- returns name string and its display width (separate because … is 3 bytes but 1 column)
+      local function displayName(bufnr)
+        if bufnr <= 0 then
+          return "", 0
+        end
+        local path = fn.bufname(bufnr)
+        if path == "" then
+          return "[No Name]", 9
+        end
+        local name = fn.fnamemodify(path, ":.")
+        if #name > maxLen then
+          return "…" .. name:sub(-(maxLen - 1)), maxLen
+        end
+        return name, #name
+      end
+
+      -- first pass over all items to get names and find max display width
+      local allNames = {}
+      local allWidths = {}
+      local maxWidth = 0
+      for i, e in ipairs(items) do
+        local name, width = "", 0
+        if e.valid == 1 then
+          name, width = displayName(e.bufnr)
+        end
+        allNames[i] = name
+        allWidths[i] = width
+        if width > maxWidth then
+          maxWidth = width
+        end
+      end
+
+      local numFmt = "%5d:%-3d"
       for i = info.start_idx, info.end_idx do
         local e = items[i]
-        local fname = ""
         local str
         if e.valid == 1 then
-          if e.bufnr > 0 then
-            fname = fn.bufname(e.bufnr)
-            if fname == "" then
-              fname = "[No Name]"
-            else
-              fname = fname:gsub("^" .. vim.env.HOME, "~")
-            end
-            -- char in fname may occur more than 1 width, ignore this issue in order to keep performance
-            if #fname <= limit then
-              fname = fnameFmt1:format(fname)
-            else
-              fname = fnameFmt2:format(fname:sub(1 - limit))
-            end
-          end
+          -- pad manually so display width is correct (not byte length)
+          local fname = allNames[i] .. string.rep(" ", maxWidth - allWidths[i])
           local lnum = e.lnum > 99999 and -1 or e.lnum
           local col = e.col > 999 and -1 or e.col
           local qtype = e.type == "" and "" or " " .. e.type:sub(1, 1):upper()
-          str = validFmt:format(fname, lnum, col, qtype, e.text)
+          str = fname .. " │" .. numFmt:format(lnum, col) .. "│" .. qtype .. " " .. e.text
         else
           str = e.text
         end

dc1de36e153e3dbc3a141b63ff255d8296ab96ff
Brian Carper <brian@carper.ca>
2026-02-24 15:20:19 -0800

    nvim remove debug

--- config/nvim/lua/plugins/substitute.nvim.lua
+++ config/nvim/lua/plugins/substitute.nvim.lua
@@ -7,6 +7,5 @@ return {
     vim.keymap.set("n", "ss", require("substitute").line, { desc = "Substitute line" })
     vim.keymap.set("n", "S", require("substitute").eol, { desc = "Substitute eol" })
     vim.keymap.set("x", "s", require("substitute").visual, { desc = "Substitute" })
-    vim.notify(vim.inspect("foo"))
   end,
 }

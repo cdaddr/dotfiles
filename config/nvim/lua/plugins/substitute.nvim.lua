fc2e5462c57c70260d010a7f094b5ce46a9d3a02
Brian Carper <brian@carper.ca>
2026-02-26 12:20:00 -0800

    nvim substitute keymaps

--- config/nvim/lua/plugins/substitute.nvim.lua
+++ config/nvim/lua/plugins/substitute.nvim.lua
@@ -3,8 +3,8 @@ return {
   lazy = false,
   config = function()
     require("substitute").setup()
-    vim.keymap.set("n", "s", require("substitute").operator, { desc = "Substitute" })
-    vim.keymap.set("n", "ss", require("substitute").line, { desc = "Substitute line" })
+    vim.keymap.set("n", "<leader>s", require("substitute").operator, { desc = "Substitute" })
+    vim.keymap.set("n", "<leader>S", require("substitute").line, { desc = "Substitute line" })
     vim.keymap.set("n", "S", require("substitute").eol, { desc = "Substitute eol" })
     vim.keymap.set("x", "s", require("substitute").visual, { desc = "Substitute" })
   end,

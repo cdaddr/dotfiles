a62ab477ac896c451d22f49a06d2b2a1a23991e2
Brian Carper <brian@carper.ca>
2026-02-26 12:19:33 -0800

    nvim nvim-tree keymaps

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -1,10 +1,20 @@
 return {
   "nvim-tree/nvim-tree.lua",
   lazy = false,
-  opts = {},
+  opts = {
+    on_attach = function(bufnr)
+      local api = require("nvim-tree.api")
+
+      local function opts(desc)
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+      api.map.on_attach.default(bufnr)
+      vim.keymap.set("n", "<C-[>", api.tree.change_root_to_parent, opts("Up"))
+    end,
+  },
     -- stylua: ignore
   keys = {
-    { "<d-e>", function() require("nvim-tree.api").tree.open({ path = "." }) end, mode = { "n" }, desc = "NvimTreeOpen (cwd)", },
-    { "<d-E>", function() require("nvim-tree.api").tree.open(vim.fn.expand('%:h')) end, mode = { "n" }, desc = "NvimTreeOpen (buffer dir)", },
+    { "<d-e>", function() require("nvim-tree.api").tree.toggle({ path = "." }) end, mode = { "n" }, desc = "nvim-tree: toggle (cwd)", },
+    { "<d-E>", function() require("nvim-tree.api").tree.toggle({ path = vim.fn.expand('%:h') }) end, mode = { "n" }, desc = "nvim-tree: toggle (buffer dir)", },
   },
 }

481900775b41714e51471fd13820c1111bd3110c
Brian Carper <brian@carper.ca>
2026-03-09 17:02:40 -0700

    nvim remove email snippet, add email abbrev from env var

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -85,3 +85,6 @@ vim.api.nvim_create_user_command("CopyCodepoint", function()
   vim.fn.setreg('*', codepoint)
   vim.notify(codepoint)
 end, { desc = "Copy codepoint under cursor" })
+
+-- insert personal info from env vars
+map("ia", "!eml", function() return vim.env.EMAIL or "!eml" end, { expr = true })

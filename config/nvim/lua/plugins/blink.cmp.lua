5fc271959f6bc3a56fa5847f43336fe6ea193a77
Brian Carper <brian@carper.ca>
2026-03-24 13:55:57 -0700

    nvim blink autoshow off

--- config/nvim/lua/plugins/blink.cmp.lua
+++ config/nvim/lua/plugins/blink.cmp.lua
@@ -77,12 +77,12 @@ return {
         selection = { preselect = true, auto_insert = true },
       },
       menu = {
-        auto_show = true,
+        auto_show = false,
         auto_show_delay_ms = 100,
         draw = {
           columns = {
-            { "label",     "label_description", gap = 1 },
-            { "kind_icon", "kind",              gap = 1 },
+            { "label", "label_description", gap = 1 },
+            { "kind_icon", "kind", gap = 1 },
           },
           components = {
             kind_icon = {

0131092233968cd568436ad3755f2f18a25db7a9
Brian Carper <bria@carper.ca>
2026-04-28 12:38:00 -0700

    nvim format css

--- config/nvim/plugin/conform.nvim.lua
+++ config/nvim/plugin/conform.nvim.lua
@@ -40,6 +40,7 @@ require('conform').setup({
     javascript = { "prettierd", "prettier", stop_after_first = true },
     typescript = { "prettierd", "prettier", stop_after_first = true },
     svelte = { "prettierd", "prettier", stop_after_first = true },
+    css = { "prettierd", "prettier", stop_after_first = true },
     ["*"] = { "codespell" },
     ["_"] = { "trim_whitespace" },
   },
